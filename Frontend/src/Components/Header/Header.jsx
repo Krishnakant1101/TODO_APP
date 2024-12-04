05b8139c147b909e36731ad1948a364dcf94dc0c
krishnakant1101 <krishnakant@thoughtwin.com>
2024-12-04 20:06:03 +0530

    update ui

--- Frontend/src/Components/Header/Header.jsx
+++ Frontend/src/Components/Header/Header.jsx
@@ -13,7 +13,12 @@ function Header() {
                      justifyContent: "center",
                 
                 }}>
+                  
                 <div id='navBar'>
+
+                <img src={"https://cdn-icons-png.flaticon.com/256/6782/6782237.png"}  style={{marginLeft:"30px",filter: "sepia(50%) brightness(1.2) hue-rotate(40deg)"}} height={"120px"}/>
+                   
+                   <div id='navSecondDiv'>
                     <div className='navOptions'>
                    
                         <NavLink to='/' className={({ isActive }) => (isActive ? 'active' : '')}> TodoApp </NavLink>
@@ -25,6 +30,20 @@ function Header() {
                         </NavLink>
                     </div>
 
+                    <div className='navOptions'>
+                       
+                       <NavLink to='/CounterApp' className={({isActive})=>(isActive?'active':"")}>
+                           Table
+                       </NavLink>
+                   </div>
+
+                   <div className='navOptions'>
+                       
+                       <NavLink to='/CounterApp' className={({isActive})=>(isActive?'active':"")}>
+                           Login
+                       </NavLink>
+                   </div>
+                   </div>
                 </div>
 
             </Box>
